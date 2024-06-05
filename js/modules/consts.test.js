589ded4d0cd3685e99346c73db30c47f2fd35b9f
Scott van Looy <scott@quul.net>
2024-06-05 08:38:35 +0200

    Test file for consts

---
+++ js/modules/consts.test.js
@@ -0,0 +1,8 @@
+import { CONSTS } from './consts';
+import { expect } from 'chai';
+
+describe('CONSTS', () => {
+  it('Is an object', () => {
+    expect(typeof CONSTS).to.eq('object');
+  });
+});
